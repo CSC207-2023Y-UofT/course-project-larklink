d01329bc6a1589dd9694669aeed1264c6b0bf0bd
vincecloutier <vincentcloutier@mail.com>
2023-07-20 08:54:30 -0400

    Added note about this being UserDBRequestModel as well.

--- src/main/java/signup_and_login/UserRequestModel.java
+++ src/main/java/signup_and_login/UserRequestModel.java
@@ -1,5 +1,8 @@
 package signup_and_login;
 
+// note  in our crc model there is a UserDBRequestModel in the same layer that is identical, so I combined them
+// TODO: decide if that's a good idea
+
 public class UserRequestModel {
     private String username;
     private String password;

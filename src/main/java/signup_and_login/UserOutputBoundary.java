fcdd8cd6325d1bbb561884c0ff2ffed5009d89b4
vincecloutier <vincentcloutier@mail.com>
2023-07-20 10:11:41 -0400

    implemented basic presenter

--- src/main/java/signup_and_login/UserOutputBoundary.java
+++ src/main/java/signup_and_login/UserOutputBoundary.java
@@ -1,6 +1,6 @@
 package signup_and_login;
 
 public interface UserOutputBoundary {
-    void prepareJoinOrHostView(String user);
-    void prepareInvalidCredentialsView(String user);
+    void prepareJoinOrHostView(String userID);
+    void prepareInvalidCredentialsView(String userID);
 }
--- src/main/java/signup_and_login/UserPresenter.java
+++ src/main/java/signup_and_login/UserPresenter.java
@@ -1,14 +1,17 @@
 package signup_and_login;
 
+import javax.swing.*;
+
 public class UserPresenter implements UserOutputBoundary {
 
     @Override
-    public void prepareJoinOrHostView(UserResponseModel response) {
-        System.out.println("goes to join or host view");
+    public void prepareJoinOrHostView(String userID) {
+        System.out.println(userID +  " goes to join or host view");
+        JOptionPane.showMessageDialog(null, userID + " would go to JoinOrHostView now.");
     }
 
     @Override
-    public void prepareInvalidCredentialsView(UserResponseModel response) {
-        System.out.println("goes to invalid credentials view");
+    public void prepareInvalidCredentialsView(String userID) {
+        JOptionPane.showMessageDialog(null, "Username and Password don't match.");
     }
 }

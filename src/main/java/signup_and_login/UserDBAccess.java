61347a403b7c5c299a4fc002edb69297f91b60be
vincecloutier <vincentcloutier@mail.com>
2023-07-20 09:36:35 -0400

    added UserDBAccess

---
+++ src/main/java/signup_and_login/UserDBAccess.java
@@ -0,0 +1,15 @@
+package signup_and_login;
+
+import java.util.List;
+
+public class UserDBAccess implements UserDBGateway {
+    @Override
+    public List<UserRequestModel> loadUsers() {
+        return null;
+    }
+
+    @Override
+    public void SaveNewUser(UserRequestModel request) {
+
+    }
+}

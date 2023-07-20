41eb212679f2d664978aea0f275a28c8f604adc6
vincecloutier <vincentcloutier@mail.com>
2023-07-20 16:11:18 -0400

    removed UserID from parameters

--- src/main/java/signup_and_login/UserOutputBoundary.java
+++ src/main/java/signup_and_login/UserOutputBoundary.java
@@ -2,5 +2,5 @@ package signup_and_login;
 
 public interface UserOutputBoundary {
     void prepareJoinOrHostView(String userID);
-    void prepareInvalidCredentialsView(String userID);
+    void prepareInvalidCredentialsView();
 }

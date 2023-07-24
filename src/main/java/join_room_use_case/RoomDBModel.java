12fc424164d963ee03f4aec9f86660ce6eb9e2c9
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-07-24 18:11:20 -0400

    added roomID as instance variable and its getter

--- src/main/java/join_room_use_case/RoomDBModel.java
+++ src/main/java/join_room_use_case/RoomDBModel.java
@@ -4,13 +4,18 @@ package join_room_use_case;
 //Need to add other instance variables and getters when merging with other branches
 public class RoomDBModel {
     private final String name;
+    private final int roomID;
 
-    public RoomDBModel(String name) {
+    public RoomDBModel(String name, int roomID) {
         this.name = name;
+        this.roomID = roomID;
     }
 
     public String getName() {
         return this.name;
+    }
+    public int getRoomID() {
+        return this.roomID;
     }
 
 }

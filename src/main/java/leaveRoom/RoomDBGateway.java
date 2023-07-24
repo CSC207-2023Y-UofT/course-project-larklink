088cf12607f20211f2d1004c2de19141e0373577
Weihan Luo <78453818+weihan1@users.noreply.github.com>
2023-07-24 19:07:31 -0400

    Delete src/main/java/leaveRoom directory

--- src/main/java/leaveRoom/RoomDBGateway.java
+++
@@ -1,9 +0,0 @@
-package leaveRoom;
-
-import java.util.List;
-
-public interface RoomDBGateway {
-    List<RoomDBRequestModel> loadRooms();
-    void updateRoomActiveUsers(RoomDBRequestModel requestModel);
-    void leaveRoom(String roomId, String currUserId);
-}

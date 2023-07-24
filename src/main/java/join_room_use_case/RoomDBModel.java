6364ce4ecd49c8760e50045ee1cf07ceff96fd19
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-07-24 17:34:40 -0400

    Changed name from RoomDBResponseModel to RoomDBModel and refactored instance variable and method

---
+++ src/main/java/join_room_use_case/RoomDBModel.java
@@ -0,0 +1,16 @@
+package join_room_use_case;
+
+
+//Need to add other instance variables and getters when merging with other branches
+public class RoomDBModel {
+    private final String name;
+
+    public RoomDBModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}

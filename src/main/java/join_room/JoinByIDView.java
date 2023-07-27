c6569ad7d52710963f5ab83638d75aeb5b24fe5d
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-07-27 10:42:37 -0400

    set minimal length of room name

--- src/main/java/join_room/JoinByIDView.java
+++ src/main/java/join_room/JoinByIDView.java
@@ -2,14 +2,17 @@ package join_room;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.regex.Pattern;
 
 public class JoinByIDView {
 
+    private static final Pattern ROOM_NAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]{3,}$");
+    private static final int MIN_ROOMNAME_LENGTH = 5;
     private JTextField roomNameField;
     private final JoinByIDController joinByIDController;
-    private final int currUserID;
+    private final Integer currUserID;
 
-    public JoinByIDView(int currUserID, JoinByIDController joinByIDController){
+    public JoinByIDView(Integer currUserID, JoinByIDController joinByIDController){
         this.currUserID = currUserID;
         this.joinByIDController = joinByIDController;
     }
@@ -50,6 +53,15 @@ public class JoinByIDView {
             if (roomName.isEmpty()) {
                 JOptionPane.showMessageDialog(null, "Please Enter Room Name",
                         "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            if (!ROOM_NAME_PATTERN.matcher(roomName).matches()) {
+                JOptionPane.showMessageDialog(null, "Invalid name! Use only alphanumeric characters. Minimum length: 5", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+
+            if (roomName.length() < MIN_ROOMNAME_LENGTH) {
+                JOptionPane.showMessageDialog(null, "Name too short! Minimum length: " + MIN_ROOMNAME_LENGTH, "Error", JOptionPane.ERROR_MESSAGE);
                 return;
             }
 

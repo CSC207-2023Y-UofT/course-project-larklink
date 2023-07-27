9febd0dc23bfb7ea7ba8f9fe7724823c81f093f5
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-07-27 10:21:29 -0400

    refactored based on new RoomDB

--- src/main/java/join_room/JoinByIDInteractor.java
+++ src/main/java/join_room/JoinByIDInteractor.java
@@ -1,7 +1,8 @@
 package join_room;
 
-import database.UserDBGateway;
-import models.UserDBModel;
+import database.*;
+import models.*;
+
 
 import java.util.List;
 
@@ -19,14 +20,14 @@ public class JoinByIDInteractor implements JoinByIDInputBoundary{
 
     @Override
     public void handleJoinByID(JoinByIDRequestModel requestModel){
-        List<RoomDBModel> rooms = roomDBGateway.loadRooms();
+        List<RoomDBModel> rooms = roomDBGateway.retrieveEveryRoom();
 
         for (RoomDBModel room : rooms) {
-            if (room.getName().equals(requestModel.getRoomName())){
+            if (room.getName().equals(requestModel.getName())){
                 List<UserDBModel> users = userDBGateway.retrieveEveryUser();
                 for (UserDBModel user : users){
                     if (user.getUserID() == requestModel.getCurrUserID()){
-                        roomDBGateway.updateRoomActiveUsers(user);
+                        roomDBGateway.joinARoom(room, requestModel.getCurrUserID());
                         presenter.prepareRoomView(room.getRoomID());
                         break;
                     }

3309f775d1e004ab65e269e9d0125f5ab914edf6
vincecloutier <vincentcloutier@mail.com>
2023-07-27 14:56:29 -0400

    now closing the previous frame when we switch views

--- src/main/java/ui/View.java
+++ src/main/java/ui/View.java
@@ -7,10 +7,18 @@ public abstract class View {
     private static final String TITLE = "Larklink";
     private static final int WIDTH = 300;
     private static final int HEIGHT = 200;
+
     public static Integer userID;
     public static Integer roomID;
 
+    private static JFrame currentFrame = null;
+
     public void prepareGUI() {
+
+        if(currentFrame != null) {
+            currentFrame.dispose(); // close previous frame
+        }
+
         JFrame frame = new JFrame(TITLE);
         frame.setLayout(new BorderLayout());
         frame.setSize(WIDTH, HEIGHT);
@@ -20,6 +28,8 @@ public abstract class View {
         JPanel panel = createPanel();
         frame.add(panel, BorderLayout.CENTER);
         frame.setVisible(true);
+
+        currentFrame = frame; // store reference to current frame (for when we want to close it)
     }
 
     abstract protected JPanel createPanel();

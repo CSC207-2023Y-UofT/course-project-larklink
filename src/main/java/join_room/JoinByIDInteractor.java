9345288d7db90010bf54ddef80465d50e54e167f
vincecloutier <vincentcloutier@mail.com>
2023-07-27 22:30:36 -0400

    early return

--- src/main/java/join_room/JoinByIDInteractor.java
+++ src/main/java/join_room/JoinByIDInteractor.java
@@ -6,29 +6,29 @@ import models.*;
 
 import java.util.List;
 
-public class JoinByIDInteractor implements JoinByIDInputBoundary{
+public class JoinByIDInteractor implements JoinByIDInputBoundary {
     private final RoomDBGateway roomDBGateway;
     private final JoinByIDOutputBoundary presenter;
 
-    public JoinByIDInteractor(RoomDBGateway roomDBGateway, JoinByIDOutputBoundary presenter){
+    public JoinByIDInteractor(RoomDBGateway roomDBGateway, JoinByIDOutputBoundary presenter) {
         this.roomDBGateway = roomDBGateway;
         this.presenter = presenter;
     }
 
     @Override
-    public void handleJoinByID(JoinByIDRequestModel requestModel){
+    public void handleJoinByID(JoinByIDRequestModel requestModel) {
         List<RoomDBModel> rooms = roomDBGateway.getRooms();
         for (RoomDBModel room : rooms) {
-            if (room.getName().equals(requestModel.getName())){
+        if (room.getName().equals(requestModel.getName())) {
                 List<Integer> activeUsers = room.getActiveUsers();
                 activeUsers.add(requestModel.getUserID());
                 room.setActiveUsers(activeUsers);
                 roomDBGateway.joinARoom(requestModel.getUserID(), room);
                 presenter.prepareRoomView(room.getRoomID());
-                break;
-                    }
-                }
-        presenter.prepareFailView();
+                return; // return here so that prepareFailView is not called in this case
+            }
         }
+        // prepareFailView is only called if no matching room is found
+        presenter.prepareFailView();
     }
-
+}

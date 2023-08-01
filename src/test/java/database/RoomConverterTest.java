7fcd79e1f3a05d756bc950ce3d4a9088b21ea930
vincecloutier <vincentcloutier@mail.com>
2023-08-01 14:19:41 -0400

    see Margaret's RoomDBAccessTest

---
+++ src/test/java/database/RoomConverterTest.java
@@ -0,0 +1,6 @@
+package database;
+
+public class RoomConverterTest {
+    // copy over testJsonToObject + testObjectToJson from Margaret's RoomDBAccessTest
+    // change it slightly to conform to RoomConverter (see UserConverterTest)
+}

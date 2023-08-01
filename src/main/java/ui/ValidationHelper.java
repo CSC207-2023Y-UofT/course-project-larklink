5751c0b0cc5d26e6dafcee509063cb80eace6eaf
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-08-01 14:32:32 -0400

    implemented signUpView, LoginView, ValidationHelper

---
+++ src/main/java/ui/ValidationHelper.java
@@ -0,0 +1,64 @@
+package ui;
+
+import javax.swing.*;
+import java.util.regex.Pattern;
+
+/**
+ * A helper class to check the validation of username and password.
+ */
+public class ValidationHelper {
+    protected static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]{3,}$");
+    protected static final int MIN_PASSWORD_LENGTH = 8;
+    protected static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9]{8,}$");
+
+    protected static boolean isUsernameValid(String username){
+        if (username.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "Username field is empty!", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (!USERNAME_PATTERN.matcher(username).matches()) {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid username! Use only alphanumeric characters. Minimum length: 3",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    protected static boolean isPasswordValid(String password){
+        if (password.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "Password field is empty!", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (password.length() < MIN_PASSWORD_LENGTH) {
+            JOptionPane.showMessageDialog(null,
+                    "Password too short! Minimum length: " + MIN_PASSWORD_LENGTH,
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid password! Use only alphanumeric characters.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        return true;
+    }
+
+    protected static boolean isRepeatPasswordValid(String password, String repeatPassword){
+        if (!password.equals(repeatPassword)){
+            JOptionPane.showMessageDialog(null,
+                    "Repeat Password does not match.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+}
--- src/main/java/ui/WelcomeView.java
+++ src/main/java/ui/WelcomeView.java
@@ -5,16 +5,10 @@ import signup_and_login.user_signup.UserSignupController;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.regex.Pattern;
 
 public class WelcomeView extends View {
-    private static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]{3,}$");
-    private static final int MIN_PASSWORD_LENGTH = 8;
-    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9]{8,}$");
     private final UserLoginController loginController;
     private final UserSignupController signupController;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
 
     public WelcomeView(UserLoginController loginController, UserSignupController signupController) {
         this.loginController = loginController;
@@ -23,18 +17,12 @@ public class WelcomeView extends View {
 
     @Override
     protected JPanel createPanel() {
-        JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
-        panel.setBorder(BorderFactory.createEmptyBorder(90, 70, 90, 70));
+        JPanel panel = new JPanel(new GridLayout(1, 2, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(130, 70, 130, 70));
 
-        this.usernameField = new JTextField();
-        this.passwordField = new JPasswordField();
         JButton signupButton = createSignUpButton();
         JButton loginButton = createLoginButton();
 
-        panel.add(new JLabel("Username:"));
-        panel.add(usernameField);
-        panel.add(new JLabel("Password:"));
-        panel.add(passwordField);
         panel.add(signupButton);
         panel.add(loginButton);
         return panel;
@@ -43,12 +31,8 @@ public class WelcomeView extends View {
     private JButton createSignUpButton() {
         JButton signupButton = new JButton("Sign up");
         signupButton.addActionListener(e -> {
-            String username = usernameField.getText();
-            String password = new String(passwordField.getPassword());
-
-            if (welcomeViewHelper(username, password)){
-                signupController.formatAndHandleUserSignup(username, password);
-            }
+            SignupView signUpView = new SignupView(this.signupController);
+            signUpView.prepareGUI();
         });
         return signupButton;
     }
@@ -56,52 +40,13 @@ public class WelcomeView extends View {
     private JButton createLoginButton() {
         JButton loginButton = new JButton("Log in");
         loginButton.addActionListener(e -> {
-            String username = usernameField.getText();
-            String password = new String(passwordField.getPassword());
-
-            if (welcomeViewHelper(username, password)){
-                loginController.formatAndHandleUserLogin(username, password);
-            }
+            LoginView logInView = new LoginView(this.loginController);
+            logInView.prepareGUI();
         });
         return loginButton;
 
     }
 
-    private boolean welcomeViewHelper(String username, String password){
 
-        if (username.isEmpty()) {
-            JOptionPane.showMessageDialog(null,
-                    "Username field is empty!", "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
 
-        if (!USERNAME_PATTERN.matcher(username).matches()) {
-            JOptionPane.showMessageDialog(null,
-                    "Invalid username! Use only alphanumeric characters. Minimum length: 3",
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-
-        if (password.isEmpty()) {
-            JOptionPane.showMessageDialog(null,
-                    "Password field is empty!", "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-
-        if (password.length() < MIN_PASSWORD_LENGTH) {
-            JOptionPane.showMessageDialog(null,
-                    "Password too short! Minimum length: " + MIN_PASSWORD_LENGTH,
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-
-        if (!PASSWORD_PATTERN.matcher(password).matches()) {
-            JOptionPane.showMessageDialog(null,
-                    "Invalid password! Use only alphanumeric characters.",
-                    "Error", JOptionPane.ERROR_MESSAGE);
-            return false;
-        }
-
-        return true;
-    }
 }

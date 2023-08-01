4134fe307c1b9ef081c9ee125010e934c0e5c792
vincecloutier <vincentcloutier@mail.com>
2023-08-01 17:00:47 -0400

    100% line coverage now

--- src/test/java/database/converters/RoomConverterTest.java
+++ src/test/java/database/converters/RoomConverterTest.java
@@ -6,11 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 
@@ -54,7 +52,7 @@ public class RoomConverterTest {
         // prepare the mock room object
         when(mockRoomDBModel.getHostID()).thenReturn(100);
         when(mockRoomDBModel.getRoomName()).thenReturn("Test Room");
-        when(mockRoomDBModel.getActiveUserIDs()).thenReturn(new ArrayList<>());
+        when(mockRoomDBModel.getActiveUserIDs()).thenReturn(List.of(1, 2, 3));
         when(mockRoomDBModel.getMessageHistory()).thenReturn("Some messages");
 
         // prepare the expected JsonObject
@@ -62,7 +60,7 @@ public class RoomConverterTest {
         JsonObject roomObject = new JsonObject();
         roomObject.addProperty("host", 100);
         roomObject.addProperty("name", "Test Room");
-        roomObject.addProperty("activeUsers", "[]");
+        roomObject.addProperty("activeUsers", "[1,2,3]");
         roomObject.addProperty("messageHistory", "Some messages");
         expectedJson.add("room", roomObject);
 

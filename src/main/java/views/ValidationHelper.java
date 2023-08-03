1390a87c4d806d8521d375693d1caeedb7b23cab
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-08-03 19:44:31 -0400

    put host button logic in ValidationHelper

--- src/main/java/views/ValidationHelper.java
+++ src/main/java/views/ValidationHelper.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.util.regex.Pattern;
 
 /**
- * A helper class to check the validation of username and password.
+ * A helper class to check the validation of username, room name, and password.
  */
 public class ValidationHelper {
     protected static final Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]{3,}$");
-    protected static final int MIN_PASSWORD_LENGTH = 8;
     protected static final Pattern PASSWORD_PATTERN = Pattern.compile("^[a-zA-Z0-9]{8,}$");
+    protected static final int MIN_PASSWORD_LENGTH = 8;
+    private static final Pattern NAME_PATTERN = Pattern.compile("^[a-zA-Z0-9]{5,}$");
+    private static final int MIN_ROOM_NAME_LENGTH = 5;
 
     protected static boolean isUsernameValid(String username){
         if (username.isEmpty()) {
@@ -55,6 +57,29 @@ public class ValidationHelper {
         if (!password.equals(repeatPassword)){
             JOptionPane.showMessageDialog(null,
                     "Repeat Password does not match.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return true;
+    }
+
+    protected static boolean isRoomNameValid(String roomName) {
+        if (!NAME_PATTERN.matcher(roomName).matches()) {
+            JOptionPane.showMessageDialog(null,
+                    "Invalid Room name! Use only alphanumeric characters. Minimum length: 5",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (roomName.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "Room Name is empty!", "Error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+
+        if (roomName.length() < MIN_ROOM_NAME_LENGTH) {
+            JOptionPane.showMessageDialog(null,
+                    "Room Name too short! Minimum length: " + MIN_ROOM_NAME_LENGTH,
                     "Error", JOptionPane.ERROR_MESSAGE);
             return false;
         }

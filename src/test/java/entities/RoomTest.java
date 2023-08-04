2d3dc8cdf4750ccadde1e1acf496d87998be831c
chelkisea <cheawon.kim@mail.utoronto.ca>
2023-08-04 07:56:08 -0400

    documented, tested getters

--- src/test/java/entities/RoomTest.java
+++ src/test/java/entities/RoomTest.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * This class is for testing Room entity.
+ */
 public class RoomTest {
     private Integer testRoomID;
     private String testRoomName;
@@ -24,11 +27,16 @@ public class RoomTest {
         testMessageHistory = "hi";
     }
 
+    /**
+     * Tests setRoom method and getters.
+     */
     @Test
     public void testSetRoom() {
         Room.setRoom(testRoomID, testRoomName, testHostID,
                 testActiveUserIDs, testMessageHistory);
 
+        // checks that the values are set correctly by comparing expected and actual values
+        // also tests getters because we get actual values from getters
         assertEquals(testRoomID, Room.getRoomID());
         assertEquals(testRoomName, Room.getRoomName());
         assertEquals(testHostID, Room.getHostID());
--- src/test/java/entities/UserTest.java
+++ src/test/java/entities/UserTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * This class is for testing User entity.
+ */
 public class UserTest {
     private Integer testUserID;
     private String testUsername;
@@ -17,20 +20,73 @@ public class UserTest {
         testPassword = "myPassword";
     }
 
+    /**
+     * Tests setUser method and getters
+     */
     @Test
     public void testSetUser() {
         User.setUser(testUserID, testUsername, testPassword);
 
+        // checks that the values are set correctly by comparing expected and actual values
         assertEquals(testUserID, User.getUserID());
         assertEquals(testUsername, User.getUsername());
         assertTrue(User.checkPassword(testPassword, User.getPassword()));
     }
 
+    /**
+     * Tests getUserID method.
+     */
+    @Test
+    public void testGetUserID() {
+        User.setUser(testUserID, testUsername, testPassword);
+        // checks that expected value is equal to actual value from getUserID
+        assertEquals(testUserID, User.getUserID());
+    }
+
+    /**
+     * Tests getUsername method.
+     */
+    @Test
+    public void testGetUsername() {
+        User.setUser(testUserID, testUsername, testPassword);
+        // checks that expected value is equal to actual value from getUsername
+        assertEquals(testUsername, User.getUsername());
+    }
+
+    /**
+     * Tests getPassword method.
+     */
+    @Test
+    public void testGetPassword() {
+        User.setUser(testUserID, testUsername, testPassword);
+        assertTrue(User.checkPassword(testPassword, User.getPassword()));
+}
+
+    /**
+     * Tests hashPassword method.
+     */
+    @Test
+    public void testHashPassword() {
+        String hashedPassword = User.hashPassword(testPassword);
+
+        // checks that hashed password is not empty
+        assertNotNull(hashedPassword);
+        // checks that hashed password and plain password are different
+        assertNotEquals(testPassword, hashedPassword);
+        // checks that checkPassword returns true for matching passwords
+        assertTrue(User.checkPassword(testPassword, hashedPassword));
+    }
+
+    /**
+     * Tests checkPassword method.
+     */
     @Test
     public void testCheckPassword() {
         String hashedPassword = User.hashPassword(testPassword);
 
+        // checks that the checkPassword returns true for matching passwords
         assertTrue(User.checkPassword(testPassword, hashedPassword));
+        // checks that the checkPassword returns true for non-matching passwords
         assertFalse(User.checkPassword("fakePassword", hashedPassword));
     }
 

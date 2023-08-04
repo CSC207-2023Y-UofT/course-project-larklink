96d0da78350743415ac50820c676f03d066a1bfa
weihan <weihan.luo@mail.utoronto.ca>
2023-08-04 12:43:26 -0400

    added simple comments to messagepresentertest

--- src/test/java/use_cases_and_adapters/messaging/MessagePresenterTest.java
+++ src/test/java/use_cases_and_adapters/messaging/MessagePresenterTest.java
@@ -16,18 +16,19 @@ public class MessagePresenterTest {
         messagePresenter = new MessagePresenter();
         messagePresenter.setView(mockView);
     }
-
+    // Test method for the prepareRoomView() function of the MessagePresenter
     @Test
     void testPrepareRoomView() {
         String messageHistory = "Old message\\nNew message";
-
         messagePresenter.prepareRoomView(messageHistory);
+        // Verify that the prepareGUI() method of the mock view is called once with the expected argument
         verify(mockView, times(1)).prepareGUI(messageHistory);
     }
-
+    // Test method for the prepareMessageErrorView() function of the MessagePresenter
     @Test
     void testPrepareMessageErrorView() {
         messagePresenter.prepareMessageErrorView();
+        // Verify that the displayPopUpMessage() method of the mock view is called once with the expected error message
         verify(mockView, times(1)).displayPopUpMessage("You cannot send an empty message!");
     }
 }

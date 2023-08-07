a585b8a5b8308e2911b337794924061963a4bae5
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2023-08-07 12:53:13 -0400

    commenting and finishing host room test

--- src/test/java/use_cases_and_adapters/host_room/HostRoomInteractorTest.java
+++ src/test/java/use_cases_and_adapters/host_room/HostRoomInteractorTest.java
@@ -1,31 +1,18 @@
 package use_cases_and_adapters.host_room;
 
-import entities.Room;
 import entities.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import use_cases_and_adapters.RoomDBModel;
-import use_cases_and_adapters.UserDBModel;
-import use_cases_and_adapters.signup_and_login.UserModel;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import use_cases_and_adapters.host_room.HostRoomDBGateway;
-import use_cases_and_adapters.host_room.HostRoomOutputBoundary;
-import use_cases_and_adapters.host_room.HostRoomInteractor;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
+/**
+ * Test host room interactor (as the name suggests)
+ */
 
 class HostRoomInteractorTest {
     private HostRoomDBGateway database;
@@ -39,6 +26,10 @@ class HostRoomInteractorTest {
         interactor = new HostRoomInteractor(database, presenter);
     }
 
+    /**
+     * Ensure that we can add rooms to DB and that the room view is shown
+     * when this is done
+     */
     @Test
     void testHostRoom_NewRoom() {
         String roomName = "TestRoom";
@@ -47,21 +38,54 @@ class HostRoomInteractorTest {
 
         interactor.hostRoom(roomName);
 
+        // room added to db
         verify(database, times(1)).updateARoom(any(RoomDBModel.class));
+        // correct view displayed
         verify(presenter, times(1)).prepareRoomView(anyString());
     }
 
+    /**
+     * Verify duplicate room name displayed when we try to create two rooms with
+     * the same name
+     */
+
     @Test
     void testHostRoom_DuplicateRoomName() {
-        String roomName = "ExistingRoom";
+        String roomName = "CoolName";
         List<RoomDBModel> existingRooms = new ArrayList<>();
+        // create a new room
         existingRooms.add(new RoomDBModel(1, roomName, 1, new ArrayList<>(), ""));
+        // return a list with the existing room when db is queried
         when(database.getRooms()).thenReturn(existingRooms);
 
         interactor.hostRoom(roomName);
-
+        // we should not add the room again because there would be duplicate names
         verify(database, never()).updateARoom(any(RoomDBModel.class));
+        // ensure that the duplicate name view is displayed
         verify(presenter, times(1)).prepareDuplicateNameView();
+    }
+
+
+    /**
+     * Ensure that there cannot be two rooms hosted by the same user
+     */
+    @Test
+    void testHostRoom_MultipleHostingView() {
+        String roomName = "NewRoom";
+        List<RoomDBModel> existingRooms = new ArrayList<>();
+        // put room with hostid 1 in db (this will collide with new attempt)
+        existingRooms.add(new RoomDBModel(1, "room1", 1, new ArrayList<>(), ""));
+        when(database.getRooms()).thenReturn(existingRooms);
+
+        // don't switch views when called
+        doNothing().when(presenter).prepareMultipleHostingView(anyString());
+        User.setUser(1, "username", "password");
+        interactor.hostRoom(roomName);
+
+        // we shouldn't have added a new room
+        verify(database, never()).updateARoom(any(RoomDBModel.class));
+        // called prepareMultipleHostingView as required
+        verify(presenter, times(1)).prepareMultipleHostingView("room1");
     }
 
 }
--- src/test/java/use_cases_and_adapters/host_room/HostRoomPresenterTest.java
+++ src/test/java/use_cases_and_adapters/host_room/HostRoomPresenterTest.java
@@ -3,12 +3,15 @@ package use_cases_and_adapters.host_room;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import use_cases_and_adapters.leave_room.LeaveRoomPresenter;
 import views.View;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+
+/**
+ * Testing the host room presenter (as the name suggests)
+ */
 public class HostRoomPresenterTest {
     private HostRoomPresenter hostRoomPresenter;
     private View mockView;
@@ -20,9 +23,39 @@ public class HostRoomPresenterTest {
         hostRoomPresenter.setView(mockView);
     }
 
+    /**
+     * Ensure that prepareGUI called when room view prepared
+     */
     @Test
     void testPrepareJoinOrHostView() {
         hostRoomPresenter.prepareRoomView("");
-        verify(mockView, times(1)).prepareGUI(""); // check that prepareGUI was called
+        // check prepareGUI called
+        verify(mockView, times(1)).prepareGUI("");
+    }
+
+
+    /**
+     * Ensure that the correct view is displayed when a user attempts
+     * to host two rooms simultaneously
+     */
+    @Test
+    void testPrepareMultipleHostingView() {
+        hostRoomPresenter.prepareMultipleHostingView("11bestnumber");
+        // string format same as message in presenter
+        verify(mockView, times(1)).displayPopUpMessage(
+                "You're already hosting a room: 11bestnumber");
+    }
+
+
+    /**
+     * Ensure that the correct view is displayed when a user attempts
+     * to host a room with a duplicate name (i.e. there already exists a room using that name)
+     */
+    @Test
+    void testPrepareDuplicateNameView() {
+        hostRoomPresenter.prepareDuplicateNameView();
+        verify(mockView, times(1)).displayPopUpMessage(
+                "A room with this name already exists.");
+
     }
 }
